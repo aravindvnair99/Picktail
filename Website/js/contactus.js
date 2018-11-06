9096984a0a8cc09200faff7dd3bc628fca5edf75
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2018-11-06 14:10:23 +0530

    Init

---
+++ Website/js/contactus.js
@@ -0,0 +1,58 @@
+﻿var mainVM;
+$(document).ready(function () {
+    mainVM = new ClassViewModel();
+
+    ko.applyBindings(mainVM, $('#body')[0]);
+});
+
+function ClassViewModel() {
+    self = this;
+    this.email = ko.observable('');
+    this.phone = ko.observable('');
+    this.name = ko.observable('');
+    this.message = ko.observable('');
+    this.subscribe = ko.observable('');
+
+    this.submit = function () {
+        
+        $.ajax({
+            type: "POST",
+            url: "Default.aspx/sendContact",
+            data: "{'email':'" + this.email() + "','phone':'" + this.phone() + "','name':'" + this.name() + "','message':'" + this.message() + "'}",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function () {
+                self.email('');
+                self.message('');
+                self.name('');
+                self.phone('');
+                alert("Enquiry Recieved!!");
+            },
+            error: function (msg) {
+                alert("error");
+            }
+        });
+
+    }
+    this.ssubmit = function () {
+
+        $.ajax({
+            type: "POST",
+            url: "Default.aspx/sendSubscribe",
+            data: "{'subscribe':'" + this.subscribe() + "'}",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function () {
+                self.subscribe('');
+                
+                alert("Subscribed!!");
+            },
+            error: function (msg) {
+                alert("error");
+            }
+        });
+
+    }
+
+
+}
---
+++ Website/js/knockoutCustomBindings.js
@@ -0,0 +1,134 @@
+﻿/* Knockout numeric binding */
+ko.bindingHandlers.numeric = {
+    init: function (element) {
+        $(element).on("keydown", function (event) {
+            // Allow: backspace, delete, tab, escape, and enter
+            if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 ||
+                // Allow: Ctrl+A
+                (event.keyCode == 65 && event.ctrlKey === true) ||
+                // Allow: . ,
+                (event.keyCode == 188 || event.keyCode == 190 || event.keyCode == 110) ||
+                // Allow: home, end, left, right
+                (event.keyCode >= 35 && event.keyCode <= 39)) {
+                // let it happen, don't do anything
+                return;
+            }
+            else {
+                // Ensure that it is a number and stop the keypress
+                if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+                    event.preventDefault();
+                }
+            }
+        });
+    }
+};
+
+/* Knockout currency binding */
+ko.bindingHandlers.currency = {
+    symbol: ko.observable('$'),
+    update: function (element, valueAccessor, allBindingsAccessor) {
+        return ko.bindingHandlers.text.update(element, function () {
+            var value = +(ko.utils.unwrapObservable(valueAccessor()) || 0),
+                symbol = ko.utils.unwrapObservable(allBindingsAccessor().symbol === undefined
+                            ? allBindingsAccessor().symbol
+                            : ko.bindingHandlers.currency.symbol);
+            return symbol + value.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
+        });
+    }
+};
+/* Knockout href binding */
+ko.bindingHandlers.href = {
+    update: function (element, valueAccessor) {
+        ko.bindingHandlers.attr.update(element, function () {
+            return { href: valueAccessor() }
+        });
+    }
+};
+/* Knockout src binding */
+ko.bindingHandlers.src = {
+    update: function (element, valueAccessor) {
+        ko.bindingHandlers.attr.update(element, function () {
+            return { src: valueAccessor() }
+        });
+    }
+};
+
+
+ko.bindingHandlers.enableAll = {
+    update: function (elem, valueAccessor) {
+        var enabled = ko.utils.unwrapObservable(valueAccessor());
+
+        ko.utils.arrayForEach(elem.getElementsByTagName('input'), function (i) {
+            i.disabled = !enabled;
+        });
+    }
+};
+ko.bindingHandlers.toggleAll = {
+    update: function(element, valueAccessor, allBindingsAccessor) {
+        var aa = allBindingsAccessor();
+        var selector = aa.selector;
+        var va = valueAccessor();
+        var value = ko.utils.unwrapObservable(va);
+        if( value )
+        {
+            $(selector).children().removeAttr('disabled');
+        }
+        else
+        {
+            $(selector).children().attr('disabled', 'disabled');
+        }
+    }
+};
+
+
+
+ko.subscribable.fn.subscribeChanged = function (callback) {
+    var previousValue;
+    this.subscribe(function (_previousValue) {
+        previousValue = _previousValue;
+    }, undefined, 'beforeChange');
+    this.subscribe(function (latestValue) {
+        callback(latestValue, previousValue);
+    });
+};
+
+
+ko.bindingHandlers.dateTimePicker = {
+    init: function (element, valueAccessor, allBindingsAccessor) {
+        //initialize datepicker with some optional options
+        var options = allBindingsAccessor().dateTimePickerOptions || {};
+        $(element).datetimepicker(options);
+
+        //when a user changes the date, update the view model
+        ko.utils.registerEventHandler(element, "dp.change", function (event) {
+            var value = valueAccessor();
+            if (ko.isObservable(value)) {
+                if (event.date != null && !(event.date instanceof Date)) {
+                    value(event.date.toDate());
+                } else {
+                    value(event.date);
+                }
+            }
+        });
+
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+            var picker = $(element).data("DateTimePicker");
+            if (picker) {
+                picker.destroy();
+            }
+        });
+    },
+    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+
+        var picker = $(element).data("DateTimePicker");
+        //when the view model is updated, update the widget
+        if (picker) {
+            var koDate = ko.utils.unwrapObservable(valueAccessor());
+
+            //in case return from server datetime i am get in this form for example /Date(93989393)/ then fomat this
+            koDate = (typeof (koDate) !== 'object') ? new Date(parseFloat(koDate.replace(/[^0-9]/g, ''))) : koDate;
+
+            picker.date(koDate);
+        }
+    }
+};
